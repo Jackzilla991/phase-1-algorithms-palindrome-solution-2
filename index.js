f5e18288899445b3db03fa1f6eb13ad65bb1726b
Jack Scarpelli <Festivetomcat99@gmail.com>
2023-01-21 17:27:35 -0500

    commit tree c8d44a8eebacb45a4b7ea41ed974abf1abeb8b0e

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  
+  const reversedWord = word.split('').reverse().join('');
+  
+  return word === reversedWord;
 }
 
-/* 
-  Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+ 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
 }
 
 module.exports = isPalindrome;
